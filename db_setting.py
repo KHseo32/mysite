87192c9b7c17e6a1c5c8f7d21e5aa885d06d8acf
KHseo32 <163965038+KHseo32@users.noreply.github.com>
2024-05-16 14:26:46 +0900

    d

--- db_setting.py
+++ db_setting.py
@@ -2,30 +2,29 @@ import pymysql
 import pymysql.cursors
 
 _db = pymysql.connect(
-        host = 'KHSeo.mysql.pythonanywhere-services.com',
-        port = 3306,
-        user = 'KHSeo',
-        password = 'tjrbgjs13^',
-        db = 'KHSeo$ubion'
+        host='KHSeo.mysql.pythonanywhere-services.com',
+        port=3306,
+        user='KHSeo',
+        password='tjrbgjs13^',
+        db='KHSeo$ubion'
         )
 
 cursor = _db.cursor(pymysql.cursors.DictCursor)
 
 create_user = """
-        create table
-        if exists
-        user(
-        id varchar(32) primary key,
-        password varchar(64) not null,
-        name varchar(32) not null
+        CREATE TABLE IF NOT EXISTS `user` (
+        id VARCHAR(32) PRIMARY KEY,
+        password VARCHAR(64) NOT NULL,
+        name VARCHAR(32) NOT NULL
         )
     """
-# sql 쿼리문 실행
+
+# Execute the SQL query
 cursor.execute(create_user)
-# 동기화
+# Commit the transaction
 _db.commit()
-# 서버와의 연결 종료
+# Close the connection
 _db.close()
 
-# 확인
-print('table 생성 완료')
+# Confirmation
+print('Table created successfully.')
